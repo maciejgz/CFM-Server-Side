e725270bfb0b1e7eb6192709638ed260ee7c430b
smieci000 <smieci000>
2014-10-10 20:46:41 +0200

    persistence.xml zelżności

--- cfm-root/cfm-commons/src/main/java/pl/mg/cfm/commons/dao/CFMDaoAbstract.java
+++ cfm-root/cfm-commons/src/main/java/pl/mg/cfm/commons/dao/CFMDaoAbstract.java
@@ -9,38 +9,35 @@ import pl.mg.cfm.pojo.CarPojo;
 
 public class CFMDaoAbstract implements CFMDao {
 
-    
-    @PersistenceContext(unitName="cfm-ejb")
+	public List<CarPojo> getAllCars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CarPojo findCar(String carId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<CarPojo> findUserCars(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean updateCarPosition(String carId, boolean lattitude,
+			double longitude) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setCarOwner(int newCarUserId, String carId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@PersistenceContext(unitName="cfm-ejb")
     private EntityManager em;
     
-    @Override
-    public List<CarPojo> getAllCars() {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
-    @Override
-    public CarPojo findCar(String carId) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public List<CarPojo> findUserCars(int userId) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public boolean updateCarPosition(String carId, boolean lattitude, double longitude) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public void setCarOwner(int newCarUserId, String carId) {
-        // TODO Auto-generated method stub
-
-    }
 
 }
