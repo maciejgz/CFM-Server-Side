d14b97d6d8705ddcccf0f5a1bfdd95cf63895394
smieci000 <smieci000>
2014-10-12 15:56:14 +0200

    -nowy persistence -ws rs

---
+++ cfm-web/src/main/java/pl/mg/cfm/ws/EmployeeService.java
@@ -0,0 +1,31 @@
+package pl.mg.cfm.ws;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import org.jboss.logging.Logger;
+
+import pl.mg.cfm.commons.dao.CFMDao;
+
+@Path("employee")
+public class EmployeeService {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(EmployeeService.class);
+
+	@EJB(beanName = "CFMDaoHibernate")
+	CFMDao dao;
+
+	@Path("/register")
+	public Response registerUser(@PathParam("firstName") String firstName,
+			@PathParam("lastName") String lastName,
+			@PathParam("roleId") int roleId,
+			@PathParam("password") String password) {
+		LOGGER.debug("register_user;firstName=" + firstName + ",lastName="
+				+ lastName + ";roleId=" + roleId + ";password=" + password);
+		return null;
+	}
+
+}
---
+++ cfm-web/src/test/java/pl/mg/cfm/ws/test/RegisterUserTest.java
@@ -0,0 +1,12 @@
+package pl.mg.cfm.ws.test;
+
+import org.junit.Test;
+
+public class RegisterUserTest {
+
+	@Test
+	public void registerUser() {
+		
+	}
+
+}
