baf29bcd0ce6dab148f7a492ef0c970e246c1f65
smieci000 <smieci000@gmail.com>
2015-05-02 14:36:38 +0200

    toString fixes: dependency issues

--- cfm-root/cfm-web-client/src/main/java/pl/mg/cfm/webclient/data/entity/Car.java
+++ cfm-root/cfm-web-client/src/main/java/pl/mg/cfm/webclient/data/entity/Car.java
@@ -2,13 +2,7 @@ package pl.mg.cfm.webclient.data.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "car")
@@ -30,7 +24,7 @@ public class Car implements Serializable {
     @Column(name="longitude")
     private Double longitude;
     
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name="car_owner_id")
     private Employee owner;
 
@@ -89,7 +83,7 @@ public class Car implements Serializable {
                 ", distance=" + distance +
                 ", latitude=" + latitude +
                 ", longitude=" + longitude +
-                ", owner=" + owner +
+                ", owner=" + owner.getIdemployee() +
                 '}';
     }
 }
--- cfm-root/cfm-web-client/src/main/java/pl/mg/cfm/webclient/data/entity/EmployeeRole.java
+++ cfm-root/cfm-web-client/src/main/java/pl/mg/cfm/webclient/data/entity/EmployeeRole.java
@@ -4,58 +4,56 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "employee_role")
+@NamedQueries({
+        @NamedQuery(name = "employeeRole.getAll", query = "SELECT e FROM EmployeeRole e")
+})
 public class EmployeeRole implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	private int id;
+    @Id
+    private int id;
 
-	@Column(name = "name")
-	private String name;
+    @Column(name = "name")
+    private String name;
 
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "role")
-	private Collection<Employee> employees = new ArrayList<Employee>();
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "role", fetch = FetchType.EAGER)
+    private Collection<Employee> employees = new ArrayList<Employee>();
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Collection<Employee> getEmployees() {
-		return employees;
-	}
+    public Collection<Employee> getEmployees() {
+        return employees;
+    }
 
-	public void setEmployees(Collection<Employee> employees) {
-		this.employees = employees;
-	}
+    public void setEmployees(Collection<Employee> employees) {
+        this.employees = employees;
+    }
 
-	@Override
-	public String toString() {
-		return "EmployeeRole{" +
-				"id=" + id +
-				", name='" + name + '\'' +
-				", employees=" + employees +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "EmployeeRole{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", employees=" + employees +
+                '}';
+    }
 }
